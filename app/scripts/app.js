dde3625c538960475fdefe572a1fa159ca549cad
Walter <wbrand@gmail.com>
2014-07-01 15:37:54 +0200

    initial setup

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,1 +1,15 @@
-angular.module('app', []);
+angular.module('app', ['ui.router']);
+
+angular.module('app').config(function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise("/");
+
+    $stateProvider.state('home', {
+        url:'/',
+        template :'<div>Ik ben de home pagina</div>'
+    });
+    $stateProvider.state('contact',{
+        url:'/contact',
+        template : '<div>Ik ben de contactpagina</div>'
+    })
+});
+
